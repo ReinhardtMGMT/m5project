232065c4018dd5ca226d05a8583881e56f70301d
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-10-31 18:22:45 -0300

    feat: models for subject done

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=60, unique=True)
